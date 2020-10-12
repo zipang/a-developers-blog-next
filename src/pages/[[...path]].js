13282963aeb1ab1af8c40a8bff2016837fa82611
zipang <christophe.Desguez@gmail.com>
2020-10-12 16:47:33 +0200

    Display some real data

--- src/pages/[[...path]].js
+++ src/pages/[[...path]].js
@@ -1,17 +1,22 @@
-import { contentDir } from "../../next.config";
-import { getStaticPathsFrom } from "../lib/next/FlatFileContentProvider.js";
+import { contentDir } from "../../next.config.js";
+import {
+	getStaticPathsFrom,
+	getStaticPropsFor
+} from "../lib/next/FlatFileContentProvider.js";
 import Layout from "../components/Layout";
+import ReactMarkdown from "react-markdown";
 
-const GenericPage = (...args) => (
+const GenericPage = ({ data, content }) => (
 	<Layout>
 		<main>
 			<section className="hero">
 				<div className="hero-body container">
-					<h2 className="title">Catch all page</h2>
+					<h2 className="title">Catch all pages</h2>
 					<div className="content">This page catches all URLs</div>
 					<code>
-						<pre>{JSON.stringify(args)}</pre>
+						<pre>{JSON.stringify(data)}</pre>
 					</code>
+					<ReactMarkdown source={content}></ReactMarkdown>
 				</div>
 			</section>
 		</main>
@@ -22,5 +27,11 @@ const GenericPage = (...args) => (
  * Scan all the /content dir to find the path to render as pages
  */
 export const getStaticPaths = getStaticPathsFrom(contentDir, "path");
+
+/**
+ * Read the `path` params which gives the route to the markdown file to load
+ * And return the file content as props
+ */
+export const getStaticProps = getStaticPropsFor("path");
 
 export default GenericPage;
