459ebed7171ac71922c0db60c80c526e3344347f
zipang <christophe.Desguez@gmail.com>
2020-10-22 13:14:58 +0200

    A simple way to load global data from static markdown files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
 import "../styles/theme.sass";
+import metadata from "../../content/_metadata/index.md";
+import headerNavigation from "../../content/_navigation/header-navigation.md";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	const enhancedProps = {
+		metadata: metadata.data,
+		navigation: {
+			header: headerNavigation.data.entries
+		},
+		...pageProps
+	};
+	if (typeof window === "undefined") console.dir(enhancedProps);
+	return <Component {...enhancedProps} />;
 }
 
 export default MyApp;
